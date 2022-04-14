acbc19af881d0a84cfdf3188f68b5462aea37098
DanielD184 <dlugolinsky555@gmail.com>
2022-04-15 01:40:10 +0200

    User for Admin + finish for User

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,8 @@
+//TODO: Main tasks
+//Get all users for User
+//Get own profile data for User
+//Everything else delete or for admin
+
 import {
 	Router,
 	Request,
@@ -6,8 +11,11 @@ import {
 } from 'express'
 
 import { models } from '../db'
+import { roleCheck } from '../auth/roleCheck'
 
 const router: Router = Router()
+
+const passport  = require('passport');
 
 const {
 	Users
@@ -17,9 +25,41 @@ export default () => {
 	router.get('/', async (_req: Request, res: Response, _next: NextFunction) => {
 		const users = await Users.findAll()
 		return res.json({
-			data: users.filter,
+			data: users,
 			message: 'List of users'
 		})
+	})
+
+
+	router.get('/:id', async (_req: Request, res: Response, _next: NextFunction) => {
+		const this_id = _req.params.id;
+		const user = await Users.findByPk(this_id)
+		return res.json({
+			data: user,
+			message: 'Details of user'
+		})
+	})
+
+	router.put('/:id', passport.authenticate('jwt', {session:false}), roleCheck, async (_req: Request, res: Response, _next: NextFunction) => {
+		const this_id = _req.params.id;
+		const { name, surname, nickName, age, role } = _req.body;
+		await Users.update({ name, surname, nickName, age, role }, {where: { id:this_id}})
+		.then(num => {
+			if (num == 1) {
+			  res.send({
+				message: "User was successfully updated!"
+			  });
+			} else {
+			  res.send({
+				message: `Cannot update user with id=${this_id}.`
+			  });
+			}
+		  })
+		  .catch(err => {
+			res.status(500).send({
+			  message: "Could not update user with id=" + this_id
+			});
+		  });
 	})
 
 	return router
