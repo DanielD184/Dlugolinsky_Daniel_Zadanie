24cddb5312a906dbe24fecc5dfa5ff72d604e9c8
DanielD184 <dlugolinsky555@gmail.com>
2022-04-15 23:49:51 +0200

    Need to think about permissions + tracking ?

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -1,14 +1,9 @@
-//TODO: track exercises he has completed (he can track same exercise multiple times, we want to save datetime of completion and duration in seconds)
-//see list of completed exercises (with datetime and duration) in profile
-//remove tracked exercise from completed exercises list
-
 import { Router, Request, Response, NextFunction } from 'express'
-import { nextTick } from 'process'
-import roleCheck from '../auth/roleCheck';
-
+	
 import { models } from '../db'
 import { USER_ROLE } from '../utils/enums'
-import { roleCheck } from '../auth/roleCheck'
+import { checkIsInRole } from '../auth/roleCheck';
+
 
 const router: Router = Router()
 const passport  = require('passport');
@@ -20,9 +15,10 @@ const {
 } = models
 
 export default () => {
-	router.get('/', async (_req: Request, res: Response, _next: NextFunction) => 
+	// Get all exercises / Admin only
+	router.get('/', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => 
 	{		
-		const exercises = await Exercise.findAll({
+		const exercises = await Exercise.findAll({ paranoid: false,
 			include: [{
 				model: Program,
 				as: 'program'
@@ -35,14 +31,15 @@ export default () => {
 		})
 	})
 
-	router.post('/', passport.authenticate('jwt', {session:false}), roleCheck, async (_req: Request, res: Response, _next: NextFunction) => {
-		const { difficulty, name , programID} = _req.body;
+	// Create new exercises / Admin only
+	router.post('/', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
+		const { difficulty, name , programID, userId} = _req.body;
 
 		const alreadyExists = await Exercise.findOne({ where: {name} });
 
 		if (alreadyExists) return res.json({ message:"Exercise already exists"});
 
-		const newExercise = new Exercise({ difficulty, name, programID })
+		const newExercise = new Exercise({ difficulty, name, programID, userId })
 		const savedExercise = await newExercise.save().catch((err) => {
 			console.log("err", err);
 			res.json({error:"Cannot create new exercise at the moment"})
@@ -52,47 +49,99 @@ export default () => {
 		else res.json({ error:"Cannot create new exercise at the moment!"});
 	})
 	
-
-	router.put('/:id', passport.authenticate('jwt', {session:false}), roleCheck, async (_req: Request, res: Response, _next: NextFunction) => {
+	// Update exercises / Admin only
+	router.put('/:id', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
 		const this_id = _req.params.id;
 		await Exercise.update(_req.body, {where: { id:this_id}})
 		.then(num => {
 			if (num == 1) {
-			  res.send({
-				message: "Exercise was successfully updated!"
-			  });
-			} else {
-			  res.send({
-				message: `Cannot update Exercise with id=${this_id}.`
-			  });
+			  	res.send({
+					message: "Exercise was successfully updated!"
+			  	});
+			} 
+			else {
+			  	res.send({
+					message: `Cannot update Exercise with id=${this_id}.`
+			  	});
 			}
-		  })
-		  .catch(err => {
+		})
+		.catch(err => {
 			res.status(500).send({
-			  message: "Could not update Exercise with id=" + this_id
+			  	message: "Could not update Exercise with id=" + this_id
 			});
-		  });
+		});
 	})
-	
-	router.delete('/delete/:id', passport.authenticate('jwt', {session:false}), roleCheck, async (_req: Request, res: Response, _next: NextFunction) => {
+
+	// End Exercise / All
+	router.put('/endexercise/:id', passport.authenticate('jwt', {session:false}), async (_req: Request, res: Response, _next: NextFunction) => {
 		const id = _req.params.id;
 		await Exercise.destroy({where: { id:id}})
 		.then(num => {
 			if (num == 1) {
-			  res.send({
-				message: "Exercise was deleted successfully!"
-			  });
-			} else {
-			  res.send({
-				message: `Cannot delete Exercise with id=${id}.`
-			  });
+			  	res.send({
+					message: "Exercise was deleted successfully!"
+			  	});
+			} 
+			else {
+				res.send({
+					message: `Cannot delete Exercise with id=${id}.`
+				});
 			}
-		  })
-		  .catch(err => {
+		})
+		.catch(err => {
 			res.status(500).send({
-			  message: "Could not delete Exercise with id=" + id
+			  	message: "Could not delete Exercise with id=" + id
 			});
-		  });
+		});
+	})
+
+	// Track exercise
+	router.put('/track/:id', passport.authenticate('jwt', {session:false}), async (_req: Request, res: Response, _next: NextFunction) => {
+		const this_id = _req.params.id;
+		const token = _req.get('Authorization').split(" ")[1];
+		const data = await Users.findOne({where:{token:token}})
+
+		await Exercise.update(_req.body, {where: { id:this_id}})
+		.then(num => {
+			if (num == 1) {
+			  	res.send({
+					message: "Exercise was successfully updated!"
+			  	});
+			} 
+			else {
+			  	res.send({
+					message: `Cannot update Exercise with id=${this_id}.`
+			  	});
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+			  	message: "Could not update Exercise with id=" + this_id,
+			});
+		});
+	})
+	
+	// Delete Exercise / Admin only
+	router.delete('/delete/:id', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
+		const id = _req.params.id;
+		await Exercise.destroy({where: { id:id}, force: true})
+		.then(num => {
+			if (num == 1) {
+			  	res.send({
+					message: "Exercise was deleted successfully!"
+			  	});
+			} 
+			else {
+			  	res.send({
+					message: `Cannot delete Exercise with id=${id}.`
+			  	});
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message: "Could not delete Exercise with id=" + id
+			});
+		});
 	})
 
 	return router
