e688d24f96efe69b2b945c58101829d59444c820
DanielD184 <dlugolinsky555@gmail.com>
2022-04-15 14:01:42 +0200

    Rolecheck for USER/ADMIN/Not logged in

--- src/auth/roleCheck.ts
+++ src/auth/roleCheck.ts
@@ -1,27 +1,29 @@
 //TODO: Return 3 values and compare User/Admin/Not Logged
 
-import { Router, Request, Response, NextFunction } from 'express'
-
 import { models } from '../db'
-import { USER_ROLE } from '../utils/enums';
-
 
 const {
 	Users
 } = models
 
-export default async (req,res,next) => {
+const checkIsInRole = (...roles) => async (req, res, next) => {
     const token = req.get('Authorization').split(" ")[1]
-	const loggedUser = await Users.findOne({where:{token:token}})
-	if (!loggedUser){
-		res.json({
-			message: 'You are not logged in!'
-		})
-	}
-	else if (loggedUser.role !== USER_ROLE.ADMIN){
-		res.json({
-			message: 'You dont have access!'
-		})
-	}
-    next()
+
+    if (!token) {
+        return res.send({
+            message: "Please log in"
+        })
+    }
+
+    const loggedUser = await Users.findOne({where:{token:token}})
+    
+    if (loggedUser.role !== roles) {
+        return res.send({
+            message: "Your role is not a " + roles
+        })
+    }
+    
+    return next()
 }
+
+export {checkIsInRole}
