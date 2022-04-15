0c0e13ac479646c06e5cfba734a486af87aa05b6
DanielD184 <dlugolinsky555@gmail.com>
2022-04-15 15:00:53 +0200

    roleCheck change error handling

--- src/auth/roleCheck.ts
+++ src/auth/roleCheck.ts
@@ -10,18 +10,19 @@ const checkIsInRole = (...roles) => async (req, res, next) => {
     const token = req.get('Authorization').split(" ")[1]
 
     if (!token) {
-        return res.send({
+        return res.status(500).send({
             message: "Please log in"
         })
     }
 
-    const loggedUser = await Users.findOne({where:{token:token}})
-    
-    if (loggedUser.role !== roles) {
-        return res.send({
-            message: "Your role is not a " + roles
-        })
-    }
+    await Users.findOne({where:{token:token}})
+    .then((loggedUser) => {
+        if (loggedUser.role !== roles) {
+            return res.status(500).send({
+                message: "Your role is not a " + roles
+            })
+        }
+    })
     
     return next()
 }
