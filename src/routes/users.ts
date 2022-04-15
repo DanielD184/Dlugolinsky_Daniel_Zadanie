6b9c8169cccb95fc43da0270081e398e02fac162
DanielD184 <dlugolinsky555@gmail.com>
2022-04-15 23:48:46 +0200

    Add Reg, Log to Users... Admin/User permissions

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,13 +5,15 @@ import {
 	NextFunction
 } from 'express'
 
-import { models } from '../db'
-import { checkIsInRole } from '../auth/roleCheck'
-import { USER_ROLE } from '../utils/enums'
+import { models } from '../db';
+import { checkIsInRole } from '../auth/roleCheck';
+import { USER_ROLE } from '../utils/enums';
+import { UserModel } from '../db/users';
 
 const router: Router = Router()
 
 const passport  = require('passport');
+const jwt = require('jsonwebtoken');
 
 const {
 	Users,
@@ -19,7 +21,8 @@ const {
 } = models
 
 export default () => {
-	router.get('/', passport.authenticate('jwt', {session:false}), async (_req: Request, res: Response, _next: NextFunction) => {
+	// Get all Users etc. / Admin only
+	router.get('/', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
 		const token = _req.get('Authorization').split(" ")[1];
 		const loggedUser = await Users.findOne({where:{token:token}})		
 
@@ -51,6 +54,7 @@ export default () => {
 		}
 	})
 
+	// Get profile / All users
 	router.get('/profile', passport.authenticate('jwt', {session:false}), async (_req: Request, res: Response, _next: NextFunction) => {
 		const token = _req.get('Authorization').split(" ")[1];
 		
@@ -76,13 +80,9 @@ export default () => {
 		})
 	})
 
+	// Find User by ID / Admin only
 	router.get('/:id', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
 		const this_id = _req.params.id;
-
-		if(res.status(500)){
-			console.error("User account problem! checkIsInRole")
-			return
-		}
 
 		await Users.findByPk(this_id)
 		.then((user) => {
@@ -98,6 +98,7 @@ export default () => {
 		})
 	})
 
+	// Update User / Admin only
 	router.put('/:id', passport.authenticate('jwt', {session:false}), checkIsInRole(USER_ROLE.ADMIN), async (_req: Request, res: Response, _next: NextFunction) => {
 		const this_id = _req.params.id;
 		const { name, surname, nickName, age, role } = _req.body;
@@ -122,6 +123,44 @@ export default () => {
 				message: "Could not update user with id=" + this_id
 			});
 		});
+	})
+
+	// Login
+	router.post('/login', async (_req: Request, res: Response, _next: NextFunction) => {
+		const { email, password } = _req.body;
+
+        const userWithEmail = await UserModel.findOne({ where: {email} }).catch((err) => {
+            console.error("Error", err);
+        });
+
+        if(!userWithEmail)
+            return res.json({ message: "Email or password doesnt match!"});
+
+        if(userWithEmail.password !== password)
+            return res.json({ message: "Email or password doesnt match!"});
+
+        const jwtToken = jwt.sign({ email: userWithEmail.email, password: userWithEmail.password }, 'test')
+        res.json({ Message: "Welcome!", token:jwtToken, role:userWithEmail.role});
+        Users.update({token : jwtToken}, {where: { email:userWithEmail.email}})
+	})
+
+	// Registration
+	router.post('/register', async (_req: Request, res: Response, _next: NextFunction) => {
+
+		const { name, surname, nickName, email, password, age, role } = _req.body;
+
+		const alreadyExists = await UserModel.findOne({ where: {email} });
+
+		if (alreadyExists) return res.json({ message:"User Email already exists"});
+
+		const newUser = new UserModel({ name, surname, nickName, email, password, age, role })
+		const savedUser = await newUser.save().catch((err) => {
+			console.log("err", err);
+			res.json({error:"Cannot register at the moment"})
+		}); 
+
+		if(savedUser) return res.json({message: 'Thanks for registering!' });
+		else res.json({ error:"Cannot register at the moment!"});
 	})
 
 	return router
